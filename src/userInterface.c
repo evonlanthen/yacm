7c49ca065ff4f90277a0834ac9b59cd7916a1bb5
eojthejudge <bauma12@bfh.ch>
2011-05-25 23:27:49 +0200

    Added some more nano-x stuff and first use of model.h

--- src/userInterface.c
+++ src/userInterface.c
@@ -1,18 +1,28 @@
 /*
- * userInterface.c
- *
- *  Created on: May 23, 2011
- *      Author: Toni Bauman
+ * @file   userInterface.c
+ * @author Toni Baumann (bauma12@bfh.ch)
+ * @date   May 23, 2011
+ * @brief  Contains the user interface.
  */
+
+#define MWINCLUDECOLORS
 
 #include "defines.h"
 #include "nano-X.h"
+#include "model.h"
+#include "userInterface.h"
+
+/* Some window related constants */
+#define WIN_BORDER	5
+
+/* Current state of display with handles and elements */
+static struct DisplayState displaystate;
 
 /**
  * Update current view on display
  * parameter should be the state construct
  */
-int updateView(int view) {
+int updateView(struct CoffeeMaker coffeemaker) {
 	return TRUE;
 }
 
@@ -25,6 +35,16 @@ int initDisplay(void) {
 	if (GrOpen() < 0) {
 		retval = FALSE;
 	}
+	/* Get the screen info */
+	GrGetScreenInfo(&displaystate.screenInfo);
+	displaystate.winSizeX = displaystate.screenInfo.cols - 2 * WIN_BORDER;
+	displaystate.winSizeY = displaystate.screenInfo.rows - 2 * WIN_BORDER;
+
+	/* Get a window handle */
+	displaystate.gWinID = GrNewWindow(GR_ROOT_WINDOW_ID, WIN_BORDER, WIN_BORDER, displaystate.winSizeX, displaystate.winSizeY, WIN_BORDER, BLACK, YELLOW);
+
+	/* Show the window */
+	GrMapWindow(displaystate.gWinID);
 	return retval;
 
 }
--- src/userInterface.h
+++ src/userInterface.h
@@ -12,7 +12,7 @@
  * Update current view on display
  * parameter should be the state construct
  */
-extern int updateView(int view);
+extern int updateView(struct CoffeeMaker coffeemaker);
 
 /**
  * Initialize display
@@ -23,6 +23,17 @@ extern int initDisplay(void);
  * turn off display and clean up
  */
 extern int shutdownDisplay(void);
+
+struct DisplayState {
+	GR_WINDOW_ID	gWinID;
+	GR_IMAGE_ID 	imageID;
+	GR_IMAGE_INFO 	imageInfo;
+	GR_GC_ID		gContextID, gTestID, gRectID, gElliID, gElli2ID;
+	GR_EVENT	   	event;
+	GR_SCREEN_INFO  screenInfo;
+	GR_FONT_ID		font;
+	int				winSizeX, winSizeY;
+};
 
 #endif /* USERINTERFACE_H_ */
 
