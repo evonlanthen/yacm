f3042b4f01c74dd43fded58b493ba9e0adad55b3
Elmar Vonlanthen <elmi78@gmx.ch>
2011-05-26 19:36:19 +0200

    Add code for led tests.

--- src/controller.c
+++ src/controller.c
@@ -7,12 +7,15 @@
  * Initializes and controls the program. Contains the entry point.
  */
 #define TONITESTS
+#undef ELMITESTS
 
 #include <stdio.h>
 
 #include "defines.h"
 #include "model.h"
 #include "userInterface.h"
+#include "machineController.h"
+#include "ledController.h"
 
 /**
  * The entry point of the program.
@@ -29,6 +32,32 @@ int main(int argc, char* argv[]) {
 	setUpDisplay();
 	printf("Hello world!\n");
 	updateView(coffeeMaker);
+#endif
+
+#ifdef ELMITESTS
+	//struct CoffeeMaker coffeeMaker;
+
+	#ifdef CARME
+		printf("Initializing for board CARME\n");
+	#elif defined(ORCHID)
+		printf("Initializing for board ORCHID\n");
+	#endif
+
+	initMachineController();
+	initLedController();
+	printf("Setting led1 on...\n");
+	updateLed(LED_1, led_on);
+	sleep(3);
+	printf("Setting led5 on...\n");
+	updateLed(LED_5, led_on);
+	sleep(3);
+	printf("Setting led1 off...\n");
+	updateLed(LED_1, led_off);
+	sleep(3);
+	printf("hello\n");
+
+	cleanUpLedController();
+	cleanUpMachineController();
 #endif
 
 	while (TRUE) {
