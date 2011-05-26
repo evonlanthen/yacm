27fb89691f7d0856e82c33152b76a1663612a012
eojthejudge <bauma12@bfh.ch>
2011-05-26 18:25:14 +0200

    Added testcode which can be removed by commenting out #define TONITESTS

--- src/controller.c
+++ src/controller.c
@@ -6,19 +6,30 @@
  *
  * Initializes and controls the program. Contains the entry point.
  */
+#define TONITESTS
 
 #include <stdio.h>
 
 #include "defines.h"
 #include "model.h"
+#include "userInterface.h"
 
 /**
  * The entry point of the program.
  */
 int main(int argc, char* argv[]) {
 	//struct CoffeeMaker coffeeMaker;
-
+#ifdef TONITESTS
+	struct CoffeeMaker coffeeMaker = {
+			.state = coffeeMaker_off,
+			.coffee.isAvailable = TRUE,
+			.milk.isAvailable = TRUE//,
+			//.products = &coffeeProductListElement
+	};
+	setUpDisplay();
 	printf("Hello world!\n");
+	updateView(coffeemaker);
+#endif
 
 	while (TRUE) {
 		// Catch and dispatch events
